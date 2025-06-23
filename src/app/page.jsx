4aba2f7a3d01c08c68b2c2eaaa2f78a8634f6968
Abhi <576abhishek.2020@gmail.com>
2025-06-23 15:46:05 +0530

    hnjwsnj

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,15 +1,18 @@
+"use client";
 import Header from '../components/Header';
 import OurService from '../components/OurService';
 import Footer from '../components/Footer';
 import BestProject from '../components/BestProject';
 import Discuss from '../components/Discuss';
 import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 
 export default function Home() {
+  const pathname = usePathname();
   return (
     <main>
       <Header />
-      <OurService />
+      <OurService key={pathname} />
       <BestProject />
       <Discuss />
       <Footer>

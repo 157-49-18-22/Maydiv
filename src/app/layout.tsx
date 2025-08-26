e14f44a266ca7a311d5ec31ff3ccbcdad2e2e4ff
Abhi <576abhishek.2020@gmail.com>
2025-08-26 12:04:29 +0530

    Add Google Analytics tracking scripts to RootLayout component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,10 @@
+import Script from 'next/script'
+
 export const metadata = {
   description: 'Maydiv Website',
   icons: {
     icon: '/Lo.png',
   },
-  
 }
 
 export default function RootLayout({
@@ -13,6 +14,21 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        {/* Google Analytics */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-WZELL05P2J"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-WZELL05P2J');
+          `}
+        </Script>
+      </head>
       <body>{children}</body>
     </html>
   )
